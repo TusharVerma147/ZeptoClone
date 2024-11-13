6d48923bde3b7f03abf5ffa886c5c50aad03870c
user <user@Apples-MacBook-Pro.local>
2024-11-13 18:45:41 +0530

    new commit for zepto

--- src/components/productList/index.js
+++ src/components/productList/index.js
@@ -11,42 +11,13 @@ import colors from '../../theme/colors';
 import {Icons} from '../../assets';
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
-const imageUrl =
-  'https://upload.wikimedia.org/wikipedia/commons/f/f0/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006_edit_1.jpg';
 
-import FastImage from 'react-native-fast-image';
 
 const ProductList = ({data}) => {
   const renderProducts = ({item, index}) => {
     return (
       <View style={styles.renderproduct}>
         <Image source={{uri: item.image}} style={styles.itemimage} />
-        {/* <View style={{height:50,width:50}} > */}
-        {/* <Image resizeMode='contain' source={{uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKfF1f7rX94ZhU2cAgYKQPDqeuNcBtR9td4Q&s"}} style={{height:'100%',width:'100%'}}/> */}
-        {/* </View> */}
-        {/* <Image source={Icons.ban6}/> */}
-        {/* <Image source={{uri: 'https://plus.unsplash.com/premium_photo-1688678097958-0620a452f0e8?q=80&w=2908&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}} style={styles.itemimage}/> */}
-        {/* <Image key={imageUrl} source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006_edit_1.jpg'}} style={{ resizeMode: 'cover', width: '100%', height: '100%' }}/> */}
-
-        {/* <FastImage
-        style={{ width: 200, height: 200 }}
-        source={{
-            uri: 'https://unsplash.it/400/400?image=1',
-            headers: { Authorization: 'someAuthToken' },
-            priority: FastImage.priority.normal,
-        }}
-        resizeMode={FastImage.resizeMode.contain}
-
-
-    /> */}
-
-        {/* <Image
-          style={styles.itemimage}
-          source={{
-            uri: 'https://reactnative.dev/img/tiny_logo.png',
-          }}
-        /> */}
-
         <View style={styles.name}>
           <Text numberOfLines={1} style={styles.des}>
             {item.name}
